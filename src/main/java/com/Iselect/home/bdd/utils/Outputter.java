cb130c374f62fe21b053bd6569c33d4964db89a3
Nicolas Luzurier <hifrcy@gmail.com>
2019-01-14 16:35:12 +0100

    modification outputter

--- src/main/java/com/Iselect/home/bdd/utils/Outputter.java
+++ src/main/java/com/Iselect/home/bdd/utils/Outputter.java
@@ -29,6 +29,42 @@ public class Outputter implements CommandLineRunner {
         LOG.info(user1 + " has been created !");
         userDao.save(user1);
         LOG.info(user1 + " has been saved !");
-        
+    
+
+     // Crée un second utilisateur et l'enregistre dans la BDD
+        User user2 = new User("Mohamed", "Project Partner", 33);
+        LOG.info("******************");
+        LOG.info(user2 + " has been created !");
+        userDao.save(user2);
+        LOG.info(user2 + " has been saved !");
+
+        // Lit les informations correspondant au second utilisateur
+        User tempUser = userDao.findById(2L).get(); /* On écrit "2L" car 
+                                                       le type de l'id est Long */
+        LOG.info("******************");
+        LOG.info("Second user's firstName is " + tempUser.getFirstName());
+        LOG.info("Second user's fonction is " + tempUser.getFonction());
+        LOG.info("Second user's age is " + tempUser.getAge());
+
+        // Liste les utilisateurs enregistrés dans la BDD
+        LOG.info("******************");
+        LOG.info("Users in list are ");
+        for(User myUser : userDao.findAll()) {
+            LOG.info(myUser.toString());
+        };
+
+        // Supprime le second utilisateur de la BDD
+        userDao.deleteById(2L); /* risque de provoquer une erreur si 
+                                tu n'as pas vidé ta table avant de relancer 
+                                ton application ! */
+
+        /*     Liste les utilisateurs enregistrés dans la BDD
+             (permet de vérifier que le second utilisateur
+             a bien été supprimé de la BDD) */
+        LOG.info("******************");
+        LOG.info("Users in list are ");
+        for(User myUser : userDao.findAll()) {
+            LOG.info(myUser.toString());
+        };
     }    
 }
